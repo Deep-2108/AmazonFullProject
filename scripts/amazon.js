b70840e449589eee9dba993222746d8b72ada7c9
Deep-2108 <deepa210805@gmail.com>
2024-12-19 23:15:28 +0530

    cart functionality added

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -46,7 +46,7 @@ products.forEach((product)=>{
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart " data-product-name="${product.name}">
             Add to Cart
           </button>
         </div>
@@ -54,3 +54,23 @@ products.forEach((product)=>{
 });
 
 document.querySelector('.js-products-grid').innerHTML=productsHTML;
+document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
+        button.addEventListener('click',()=>{
+            const productName =button.dataset.productName;
+          let matchingItem;
+          cart.forEach((item)=>{
+            if(productName===item.productName){
+                matchingItem=item;
+            }
+          });
+          if(matchingItem){
+            matchingItem.quantity+=1;
+          }else{
+            cart.push({
+                productName:productName,
+                quantity:1
+            });
+          }
+          console.log(cart);
+        });
+});
