ac2bb42565d27fb972f707f1e65fa11ddb4f76b7
Deep-2108 <deepa210805@gmail.com>
2024-12-23 21:01:37 +0530

    delivery date fixed more

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -18,10 +18,27 @@ cart.forEach((cartItem)=>{
             matchingProduct=product;
         }
     });
+
+    const deliveryOptionId=cartItem.deliveryOptionId;
+    let deliveryOption;
+    deliveryOptions.forEach((option)=>{
+      if(option.id==deliveryOptionId){
+        deliveryOption=option;
+      }
+    });
+  const today=dayjs();
+  const deliveryDate=today.add(
+    deliveryOption.deliveryDays,
+    'days'
+  );
+  const dateString=deliveryDate.format(
+    'dddd,MMMM D'
+  );
+
    cartSummaryHTML+= `
        <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
-              Delivery date: Wednesday, June 15
+              Delivery date:${dateString}
             </div>
 
             <div class="cart-item-details-grid">
@@ -52,7 +69,7 @@ cart.forEach((cartItem)=>{
                 <div class="delivery-options-title">
                   Choose a delivery option:
                 </div>
-                ${deliveryOptionsHTML(matchingProduct)}
+                ${deliveryOptionsHTML(matchingProduct,cartItem)}
               </div>
             </div>
           </div>
@@ -60,7 +77,7 @@ cart.forEach((cartItem)=>{
 });
 
 
-function deliveryOptionsHTML(matchingProduct){
+function deliveryOptionsHTML(matchingProduct,cartItem){
   let html='';
   deliveryOptions.forEach((deliveryOption)=>{
     const today=dayjs();
@@ -71,10 +88,13 @@ function deliveryOptionsHTML(matchingProduct){
     const dateString=deliveryDate.format(
       'dddd,MMMM D'
     );
-    const priceString=deliveryOption.priceCents===0 ? 'Free' : `$${formatCurrency(deliveryOption.priceCents)} - `
+    const priceString=deliveryOption.priceCents===0 ? 'Free' : `$${formatCurrency(deliveryOption.priceCents)} - `;
+
+    const isChecked=deliveryOption.id===cartItem.deliveryOptionId;
+
   html+=  `
                   <div class="delivery-option">
-                    <input type="radio" class="delivery-option-input"
+                    <input type="radio" ${isChecked ? 'checked': ''} class="delivery-option-input"
                       name="delivery-option-${matchingProduct.id}">
                     <div>
                       <div class="delivery-option-date">
