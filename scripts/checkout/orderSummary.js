972d67ca7680912a7c1066a8a359b48f253c2e97
Deep-2108 <deepa210805@gmail.com>
2024-12-24 20:29:38 +0530

    prices change render on web

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,11 +1,9 @@
 import {cart,removeFromCart,updateDeliveryOption} from  '../../data/cart.js';
 import { products,getProduct } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
-import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'
-import {deliveryOptions,getDeliveryOption} from '../../data/deliveryOptions.js'
-const today=dayjs();
-const deliveryDate=today.add(7,'days');
-deliveryDate.format('dddd, MMMM D');
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+import {deliveryOptions,getDeliveryOption} from '../../data/deliveryOptions.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 
 
 export function renderOrderSummary(){
@@ -111,10 +109,12 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
 link.addEventListener('click',()=>{
     const productId=link.dataset.productId;
     removeFromCart(productId);
+
 const container= document.querySelector(
   `.js-cart-item-container-${productId}`
   );
   container.remove();
+  renderPaymentSummary();
 });
 });
 
@@ -123,6 +123,7 @@ document.querySelectorAll('.js-delivery-option').forEach((element)=>{
     const {productId,deliveryOptionId} =element.dataset
       updateDeliveryOption(productId,deliveryOptionId);
       renderOrderSummary();
-  })
-})
+      renderPaymentSummary();
+  });
+});
 }
