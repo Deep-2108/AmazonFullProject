9aba182834fe93e8489e2d1f9e8d11622f35619a
Deep-2108 <deepa210805@gmail.com>
2024-12-29 22:00:58 +0530

    learnt about promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,8 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 // import '../data/backend-practice.js';
-
+new Promise(() => {
+    console.log("promise h");
+})
 renderOrderSummary();
 renderPaymentSummary();
